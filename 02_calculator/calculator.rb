69f2a40126a5b7f833544b69458d3c3858aa1623
Travis Wallenfang <tjwallenfang@mail.bradley.edu>
2017-07-17 22:24:46 -0500

    Added all functions and additional function tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add(a,b)
+	a+b
+end
+
+def subtract(a,b)
+	a-b
+end
+
+def sum(array)
+	total = 0
+	array.each do |number|
+		total += number
+	end
+	total
+end
+
+def multiply(a,b)
+	a*b
+end
+
+def power(a,b)
+	a**b
+end
+
+def factorial(a)
+	total = 1
+	if a == 0
+		return total
+	else
+		while a > 0 do
+			total = total*a
+			a -= 1
+		end
+
+	end
+	total
+end
