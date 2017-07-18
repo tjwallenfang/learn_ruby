ef94cd02c8f3f9ad6fbb03fe0c26fe14668f886a
Travis Wallenfang <tjwallenfang@mail.bradley.edu>
2017-07-17 23:08:17 -0500

    Added echo, shout, repeat, start_of_word, first_word. titlesize is wip

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(string)
+	string
+end
+
+def shout(string)
+	string.upcase
+end
+
+def repeat(string, number = 2)
+	new_string = string
+	(number-1).times do
+		new_string += " #{string}"
+	end
+	new_string
+end
+
+def start_of_word(string, number)
+	string[0,number]
+end
+
+def first_word(string)
+	array = string.split(" ")
+	array[0]
+end
+
+def titleize (string)
+	array = string.split(" ")
+	array.map!a do |word|
+		word.capitalize
+	end
+	array.join(" ")
+end
