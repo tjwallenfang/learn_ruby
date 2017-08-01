c6121e7c11d934605c617b2ec2d9f7bbd616229e
Travis Wallenfang <tjwallenfang@mail.bradley.edu>
2017-07-31 21:36:46 -0500

    Finished for all tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title
+		not_capitalized = ['the', 'a', 'an', 'and', 'of', 'or', 'in']
+		words = @title.split(" ")
+		words.map! do |word|
+			if not not_capitalized.include?(word) 
+				word.capitalize
+			else
+				word
+			end
+		end
+		words[0].capitalize!
+		@title = words.join(" ")
+	end
 end
