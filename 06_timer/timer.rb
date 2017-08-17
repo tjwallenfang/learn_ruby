6a5f1517869862cc74f543698754cfe4ec25eb64
Travis Wallenfang <tjwallenfang@mail.bradley.edu>
2017-08-17 14:53:01 -0500

    Seconds methods created and finished with helper method 'padded'

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,34 @@
 class Timer
   #write your code here
+  def initialize
+  	@seconds = 0
+  end
+  attr_accessor :seconds
+
+  def seconds
+  	@seconds
+  end
+
+  def time_string
+  	timeString = []
+  	hours = @seconds/3600
+  	timeString.push(padded(hours))
+  	left = @seconds - hours*3600
+  	minutes = left/60
+  	timeString.push(padded(minutes))
+  	seconds = left - minutes*60
+  	timeString.push(padded(seconds))
+  	timeString.join(":")
+  end
+
+  def padded(num)
+  	if num.to_s.length == 2
+  		num.to_s
+  	elsif num.to_s.length == 1
+  		num.to_s.prepend("0")
+  	end
+  end
+  			
+
+
 end
